360c273622f4be0fc41cd7672025ecdb0c260f87
iduk <iduk.design@gmail.com>
2021-01-26 00:54:24 +0900

    learning start

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 createApp(App)
+  .use(Buefy)
   .use(store)
   .use(router)
   .mount("#app");
